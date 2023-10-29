a77afe8d2be6ad6bbffdbf8287203f6902538a54
root <root@BettysHp>
2023-10-29 12:31:59 +0300

    yess

---
+++ print_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "sort.h"
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+        if (i != size - 1)
+            printf(", ");
+    }
+    printf("\n");
+}
