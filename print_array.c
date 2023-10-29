b99c333443ddf5fcfa58afc81888377bedde9581
root <root@BettysHp>
2023-10-29 12:34:21 +0300

    try

--- print_array.c
+++ print_array.c
@@ -1,6 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "sort.h"
 
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
 void print_array(const int *array, size_t size)
 {
     size_t i;
